5a2021f65c184d4da13a0ff35a9a7afae38e3eca
terence927 <32293533+terence927@users.noreply.github.com>
2019-04-11 21:18:38 +0800

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,42 +1,41 @@
-import requests
-import pandas as pd
 import numpy as np
+import pandas as pd
+import requests
 
-INDEXS = 25000
-INDEXE = 30000
+def main(date):
+    url = 'https://www.hkex.com.hk/eng/stat/dmstat/dayrpt/hsio'+date+'.csv'
+    resp = requests.get(url=url)
+    if resp.status_code == requests.codes.ok:
+        #print(resp.text)
+        res=resp.text
 
-url = 'https://www.hkex.com.hk/eng/stat/dmstat/dayrpt/hsio190226.csv'
-resp = requests.get(url=url)
-if resp.status_code == requests.codes.ok:
-	#print(resp.text)
-	data=resp.text
+    start = res.find("TOP 10 TRADED (BY VOLUME)")-1
+    end = res.find("Hang Seng Index Options HK$50 per point",start)-1
+    file = open('data.csv', 'w')
+    file.write(res[start-1:end-1])
+    file.close() 
+    data = pd.read_csv('data.csv',index_col=False)
+    calldata = data[data["TOP 10 TRADED (BY VOLUME)"]== 'C']
+    putdata = data[data["TOP 10 TRADED (BY VOLUME)"]== 'P']
 
-start = data.find("CONTRACT MONTH")
-end = data.find(',,,,,"',start)
-callfile = open('call.csv', 'w')
-callfile.write(data[start-1:end-1])
-callfile.close() 
+    INDEXS = data["Unnamed: 2"].min()-100
+    INDEXE = data["Unnamed: 2"].max()+100
 
-start = data.find("CONTRACT MONTH",end)
-end = data.find(',,,,,"',start)
-putfile = open('put.csv', 'w')
-putfile.write(data[start-1:end-1])
-putfile.close()
+    print(data)
+    for value in range(INDEXS,INDEXE,100):
 
-calldata = pd.DataFrame.from_csv('call.csv')
-putdata = pd.DataFrame.from_csv('put.csv')
-
-for value in range(INDEXS,INDEXE,100):
-    iv=0
-    for i,row in calldata.iterrows():
-            if value>int(row['STRIKE PRICE']):
-                point = (value-int(row['STRIKE PRICE']))
-                iv+=point*int(row['OPEN INTEREST']) #*int(row['VOLUME.2'])
-    niv=0
-    for i,row in putdata.iterrows():
-            if value<int(row['STRIKE PRICE']):
-                point = (int(row['STRIKE PRICE'])-value)
-                niv+=point*int(row['OPEN INTEREST']) #*int(row['VOLUME.2'])
-    print(value,(iv+niv)/10000000000)
-
-	
+        iv=0
+        for i,row in calldata.iterrows():
+            #if int(row['STRIKE PRICE'])>=25600:
+                if value>int(row['Unnamed: 2']):
+                    point = (value-int(row['Unnamed: 2']))
+                    iv+=point*int(row['OPEN INTEREST'])
+        niv=0
+        for i,row in putdata.iterrows():
+            #if int(row['STRIKE PRICE'])<=29000:
+                if value<int(row['Unnamed: 2']):
+                    point = (int(row['Unnamed: 2'])-value)
+                    niv+=point*int(row['OPEN INTEREST'])
+        loss = (iv+niv)/10000
+        print(value,loss)
+main('190410')
